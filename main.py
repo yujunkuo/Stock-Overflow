f5e5552d8f17c9157a316a3c4e2d026640370ded
yujunkuo <johnboy880313@gmail.com>
2021-08-12 13:19:17 +0800

    New strategy

--- main.py
+++ main.py
@@ -125,9 +125,9 @@ def broadcast():
     # 股票基本面篩選條件
     fundimental_mask = [
         # 月營收年增率 > 5%
-        final_df["(月)營收年增率(%)"] > 5,
+        # final_df["(月)營收年增率(%)"] > 5,
         # 累積營收年增率 > 5%
-        final_df["(月)累積營收年增率(%)"] > 5,
+        # final_df["(月)累積營收年增率(%)"] > 5,
     ]
 
     # 股票技術面篩選條件
@@ -157,16 +157,16 @@ def broadcast():
 
     # 股票籌碼面篩選條件
     chip_mask = [
-        # 今天成交量 > 1000 張
-        technical_strategy.volume_greater_check_df(final_df, shares_threshold=1000, days=1),
+        # 今天成交量 > 2000 張
+        technical_strategy.volume_greater_check_df(final_df, shares_threshold=2000, days=1),
         # 今天成交量不能是 2 天內最低量 (今天成交量要比昨天高)
         technical_strategy.today_volume_is_not_min_check_df(final_df, days=2),
         # 今量 > 5日均量
         technical_strategy.technical_indicator_greater_one_day_check_df(final_df, indicator_1="volume", indicator_2="mean_5_volume", days=1),
         # 5日均量 > 20日均量
         technical_strategy.technical_indicator_greater_one_day_check_df(final_df, indicator_1="mean_5_volume", indicator_2="mean_20_volume", days=1),
-        # 5日均量 > 800
-        technical_strategy.technical_indicator_constant_check_df(final_df, indicator="mean_5_volume", direction="more", threshold=800, days=1),
+        # 5日均量 > 1000 (持續兩天)
+        technical_strategy.technical_indicator_constant_check_df(final_df, indicator="mean_5_volume", direction="more", threshold=1000, days=2),
         # 單一法人至少買超成交量的 10%
         chip_strategy.single_institutional_buy_check_df(final_df, single_volume_threshold=10),
         # 三大法人合計買超至少超過成交量的 10%
@@ -177,11 +177,11 @@ def broadcast():
     final_filter = helper.df_mask_helper(final_df, fundimental_mask + technical_mask + chip_mask)
     final_filter = final_filter.sort_values(by=['成交股數'], ascending=False)
     # 轉換為字串回傳
-    final_recommendation_text = f"滿足條件的股票共有: {final_filter.shape[0]} 檔 (依照成交量由大到小排序)\n"
+    final_recommendation_text = f"滿足條件的股票共有: {final_filter.shape[0]} 檔 (依照成交量由大到小排序)\n\n"
     print(f"滿足條件的股票共有: {final_filter.shape[0]} 檔 (依照成交量由大到小排序)\n")
     for i, v in final_filter.iterrows():
         final_recommendation_text += f"{i} {v['名稱']}  {v['產業別']}\n"
-    final_recommendation_text += f"此清單係依據台股於 {str(final_date)} 成交資料所做之推薦\nKuo."
+    final_recommendation_text += f"\n此清單係依據台股於 {str(final_date)} 成交資料所做之推薦"
     # 透過 LINE API 進行推播
     line_bot_api.broadcast(TextSendMessage(text=final_recommendation_text))
     print("Broadcast Sucess!")
