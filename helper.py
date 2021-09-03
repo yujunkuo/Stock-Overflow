beca5ba284608dbd990c2f42b7c90866ce83a2b0
yujunkuo <johnboy880313@gmail.com>
2021-09-03 15:42:08 +0800

    Modify wackup api to check update and broadcast everytime (1 call/hr)

--- helper.py
+++ helper.py
@@ -19,8 +19,8 @@ def df_mask_helper(df, mask_list):
 # 計算當前時間是否介於兩時間之間
 def check_time_between(begin_time, end_time, check_time=datetime.datetime.now().time()):
     if begin_time < end_time:
-        return check_time >= begin_time and check_time <= end_time
+        return check_time >= begin_time and check_time < end_time
     else: # 跨過午夜
-        return check_time >= begin_time or check_time <= end_time
+        return check_time >= begin_time or check_time < end_time
     
     
