5ea313a0c64953a16b1a033d1a5ecb9c40b084e4
yujunkuo <johnboy880313@gmail.com>
2023-12-17 21:33:56 +0800

    Add the retry machanism to the crawler

--- other.py
+++ other.py
@@ -11,54 +11,69 @@ from functools import reduce
 
 ## 取得其他股票相關指標
 
+MAX_REQUEST_RETRIES = 5
+
 # (Public) 透過 FinMind 取得股票產業別
 def get_industry_category() -> pd.DataFrame:
-    parameter = {
-        "dataset": "TaiwanStockInfo",
-        "token": "", # 參考登入，獲取金鑰
-    }
-    r = requests.get("https://api.finmindtrade.com/api/v4/data", params=parameter)
-    data = r.json()
-    df = pd.DataFrame(data["data"])
-    # 去除各個欄位名稱後方的多餘空格
-    df.columns = [each.strip() for each in df.columns]
-    # 重新命名欄位
-    df = df.rename(columns={"industry_category": "產業別", "stock_id": "代號", "stock_name": "名稱", "type": "股票類型"})
-    # 更新名稱與代號欄位的資料型態
-    df["名稱"] = df["名稱"].astype(str)
-    df["代號"] = df["代號"].astype(str)
-    # 去除名稱與代號的前後空格
-    df["名稱"] = df["名稱"].str.strip()
-    df["代號"] = df["代號"].str.strip()
-    # 取出股票（4碼）
-    df = df[(df["代號"].str.len() == 4) & (df["代號"].str[:2] != "00") & (df["代號"].str.isdigit())]
-    # 只保留所需欄位
-    df = df[["代號", "名稱", "產業別", "股票類型"]]
-    df = df.set_index("代號")
-    return df
+    for _ in range(MAX_REQUEST_RETRIES):
+        try:
+            parameter = {
+                "dataset": "TaiwanStockInfo",
+                "token": "", # 參考登入，獲取金鑰
+            }
+            r = requests.get("https://api.finmindtrade.com/api/v4/data", params=parameter)
+            data = r.json()
+            df = pd.DataFrame(data["data"])
+            # 去除各個欄位名稱後方的多餘空格
+            df.columns = [each.strip() for each in df.columns]
+            # 重新命名欄位
+            df = df.rename(columns={"industry_category": "產業別", "stock_id": "代號", "stock_name": "名稱", "type": "股票類型"})
+            # 更新名稱與代號欄位的資料型態
+            df["名稱"] = df["名稱"].astype(str)
+            df["代號"] = df["代號"].astype(str)
+            # 去除名稱與代號的前後空格
+            df["名稱"] = df["名稱"].str.strip()
+            df["代號"] = df["代號"].str.strip()
+            # 取出股票（4碼）
+            df = df[(df["代號"].str.len() == 4) & (df["代號"].str[:2] != "00") & (df["代號"].str.isdigit())]
+            # 只保留所需欄位
+            df = df[["代號", "名稱", "產業別", "股票類型"]]
+            df = df.set_index("代號")
+            return df
+        except:
+            print(f"Attempt {get_industry_category.__name__} failed.")
+            time.sleep(3)
+    return pd.DataFrame(columns=["代號", "名稱", "產業別", "股票類型"]).set_index("代號")
 
 
 # (Public) 取得上市上櫃 MoM 與 YoY
 def get_mom_yoy() -> pd.DataFrame:
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-    }
-    r = requests.get("https://stock.wespai.com/p/44850", headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-    data = soup.find_all("td")
-    mom_yoy_list = [[data[x].text, data[x+1].select_one("a").text, data[x+3].text, data[x+4].text, data[x+5].text] for x in range(0, len(data), 6)]
-    df = pd.DataFrame(mom_yoy_list, columns=["代號", "名稱", "(月)營收月增率(%)", "(月)營收年增率(%)", "(月)累積營收年增率(%)"])
-    # 字串轉數字，去除逗號
-    df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", ""), errors='coerce') if s.name not in ["代號", "名稱"] else s)
-    # 更新名稱與代號欄位的資料型態
-    df["名稱"] = df["名稱"].astype(str)
-    df["代號"] = df["代號"].astype(str)
-    # 去除名稱與代號的前後空格
-    df["名稱"] = df["名稱"].str.strip()
-    df["代號"] = df["代號"].str.strip()
-    df = df.set_index("代號")
-    df = df.sort_index()
-    return df
+    for _ in range(MAX_REQUEST_RETRIES):
+        try:
+            headers = {
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+            }
+            r = requests.get("https://stock.wespai.com/p/44850", headers=headers)
+            soup = BeautifulSoup(r.text, "html.parser")
+            data = soup.find_all("td")
+            mom_yoy_list = [[data[x].text, data[x+1].select_one("a").text, data[x+3].text, data[x+4].text, data[x+5].text] for x in range(0, len(data), 6)]
+            df = pd.DataFrame(mom_yoy_list, columns=["代號", "名稱", "(月)營收月增率(%)", "(月)營收年增率(%)", "(月)累積營收年增率(%)"])
+            # 字串轉數字，去除逗號
+            df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", ""), errors='coerce') if s.name not in ["代號", "名稱"] else s)
+            # 更新名稱與代號欄位的資料型態
+            df["名稱"] = df["名稱"].astype(str)
+            df["代號"] = df["代號"].astype(str)
+            # 去除名稱與代號的前後空格
+            df["名稱"] = df["名稱"].str.strip()
+            df["代號"] = df["代號"].str.strip()
+            df = df.set_index("代號")
+            df = df.sort_index()
+            return df
+        except:
+            print(f"Attempt {get_mom_yoy.__name__} failed.")
+            time.sleep(3)
+    return pd.DataFrame(columns=["代號", "名稱", "(月)營收月增率(%)", "(月)營收年增率(%)", "(月)累積營收年增率(%)"]).set_index("代號")
+
 
 
 # (Public) 將「技術指標」添加至輸入的 DataFrame 當中
@@ -96,30 +111,32 @@ def get_technical_indicators(input_df: pd.DataFrame) -> pd.DataFrame:
 
 # 取得單一股票的技術指標資訊
 def _get_technical_indicators_from_stock_id(stock_id: str) -> dict:
-    try:
-        # days = 240 時 Render 記憶體會爆掉
-        # r = requests.get(f"https://histock.tw/stock/chip/chartdata.aspx?no={stock_id}&days=240&m=dailyk,close,volume,mean5,mean10,mean20,mean60,mean120,mean5volume,mean20volume,k9,d9,rsi6,rsi12,dif,macd,osc")
-        r = requests.get(f"https://histock.tw/stock/chip/chartdata.aspx?no={stock_id}&days=120&m=dailyk,close,volume,mean5,mean10,mean20,mean60,mean5volume,mean20volume,k9,d9,dif,macd,osc")
-        technical_data = r.json()
-        k9 = _make_technical_pretty_list(json.loads(technical_data["K9"]))
-        d9 = _make_technical_pretty_list(json.loads(technical_data["D9"]))
-        dif = _make_technical_pretty_list(json.loads(technical_data["DIF"]))
-        macd = _make_technical_pretty_list(json.loads(technical_data["MACD"]))
-        osc = _make_technical_pretty_list(json.loads(technical_data["OSC"]))
-        mean5 = _make_technical_pretty_list(json.loads(technical_data["Mean5"]))
-        mean10 = _make_technical_pretty_list(json.loads(technical_data["Mean10"]))
-        mean20 = _make_technical_pretty_list(json.loads(technical_data["Mean20"]))
-        mean60 = _make_technical_pretty_list(json.loads(technical_data["Mean60"]))
-        volume = _make_technical_pretty_list(json.loads(technical_data["Volume"]))
-        mean_5_volume = _make_technical_pretty_list(json.loads(technical_data["Mean5Volume"]))
-        mean_20_volume = _make_technical_pretty_list(json.loads(technical_data["Mean20Volume"]))
-        daily_k = _make_daily_k_pretty_list(json.loads(technical_data["DailyK"]))
-        return {"k9": k9, "d9": d9, "dif": dif, "macd": macd, "osc": osc,
-                "mean5": mean5, "mean10": mean10, "mean20": mean20, "mean60": mean60,
-                "volume": volume, "mean_5_volume": mean_5_volume, "mean_20_volume": mean_20_volume,
-                "daily_k": daily_k}
-    except:
-        return None
+    for _ in range(max(1, MAX_REQUEST_RETRIES - 2)):
+        try:
+            # days = 240 時 Render 記憶體會爆掉
+            # r = requests.get(f"https://histock.tw/stock/chip/chartdata.aspx?no={stock_id}&days=240&m=dailyk,close,volume,mean5,mean10,mean20,mean60,mean120,mean5volume,mean20volume,k9,d9,rsi6,rsi12,dif,macd,osc")
+            r = requests.get(f"https://histock.tw/stock/chip/chartdata.aspx?no={stock_id}&days=120&m=dailyk,close,volume,mean5,mean10,mean20,mean60,mean5volume,mean20volume,k9,d9,dif,macd,osc")
+            technical_data = r.json()
+            k9 = _make_technical_pretty_list(json.loads(technical_data["K9"]))
+            d9 = _make_technical_pretty_list(json.loads(technical_data["D9"]))
+            dif = _make_technical_pretty_list(json.loads(technical_data["DIF"]))
+            macd = _make_technical_pretty_list(json.loads(technical_data["MACD"]))
+            osc = _make_technical_pretty_list(json.loads(technical_data["OSC"]))
+            mean5 = _make_technical_pretty_list(json.loads(technical_data["Mean5"]))
+            mean10 = _make_technical_pretty_list(json.loads(technical_data["Mean10"]))
+            mean20 = _make_technical_pretty_list(json.loads(technical_data["Mean20"]))
+            mean60 = _make_technical_pretty_list(json.loads(technical_data["Mean60"]))
+            volume = _make_technical_pretty_list(json.loads(technical_data["Volume"]))
+            mean_5_volume = _make_technical_pretty_list(json.loads(technical_data["Mean5Volume"]))
+            mean_20_volume = _make_technical_pretty_list(json.loads(technical_data["Mean20Volume"]))
+            daily_k = _make_daily_k_pretty_list(json.loads(technical_data["DailyK"]))
+            return {"k9": k9, "d9": d9, "dif": dif, "macd": macd, "osc": osc,
+                    "mean5": mean5, "mean10": mean10, "mean20": mean20, "mean60": mean60,
+                    "volume": volume, "mean_5_volume": mean_5_volume, "mean_20_volume": mean_20_volume,
+                    "daily_k": daily_k}
+        except:
+            continue
+    return None
 
 
 # 資料清洗 (一般技術指標)
