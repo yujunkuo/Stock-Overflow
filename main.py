7c0762dc0afb551f90560a275911c99e8e28665d
Yu-Jun Kuo <johnboy880313@gmail.com>
2024-01-26 02:30:32 +0800

    test (get the version of each package)

--- main.py
+++ main.py
@@ -481,7 +481,12 @@ def get_latest_recommendations():
     print("=== [推薦觀察] 股票清單取得完成 ===")
     return
 
+try: from pip._internal.operations import freeze
+except ImportError: # pip < 10.0
+    from pip.operations import freeze
 
+pkgs = freeze.freeze()
+for pkg in pkgs: print(pkg)
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 10000))
