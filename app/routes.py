856d31c4e0954729a63df5110ef52e6c39a13530
yujunkuo <johnboy880313@gmail.com>
2024-10-03 00:59:20 +0800

    add API document & add target_date and need_broadcast headers into request

--- app/routes.py
+++ app/routes.py
@@ -5,41 +5,78 @@ import threading
 
 from config import logger
 from .views import update_and_broadcast
-from flask import abort, current_app, request, Response
+from flask import current_app, request, Response
 from linebot.exceptions import InvalidSignatureError
 
 
 def init_routes(app):
-    # Line Bot Testing route
+    
+    @app.route("/", methods=["GET"])
+    def home():
+        """
+        Default route for the application.
+
+        Responses:
+        - 200 OK: Indicates the server is up and running.
+        """
+        return Response(status=200)
+    
+    
     @app.route("/callback", methods=["POST"])
     def callback():
-        signature = request.headers["X-Line-Signature"]
+        """
+        Handle incoming webhook events from LINE Bot.
+
+        Headers:
+        - `X-Line-Signature`: Signature for request verification.
+
+        Responses:
+        - 200 OK: If the request is successfully processed.
+        - 400 Bad Request: If `X-Line-Signature` is missing or invalid.
+        """
+        # Extracting signature and body from request
+        signature = request.headers.get("X-Line-Signature")
         body = request.get_data(as_text=True)
-        app.logger.info("Request body: " + body)
+        app.logger.info(f"Request body: {body}")
         try:
             handler = current_app.config["WEBHOOK_HANDLER"]
             handler.handle(body, signature)
         except InvalidSignatureError:
-            abort(400)
-        return "OK"
-
-    # Default route
-    @app.route("/", methods=["GET"])
-    def home():
+            return Response("Invalid signature", status=400)
         return Response(status=200)
 
-    # Wakeup route - for server health check and memory clean
+
     @app.route("/wakeup", methods=["GET"])
     def wakeup():
+        """
+        Wake up the service and check memory usage.
+
+        Responses:
+        - 200 OK: If the request is successfully processed.
+        """
         gc.collect()
         process = psutil.Process()
         memory_usage = process.memory_info().rss / 1024**2
         logger.info(f"目前記憶體使用量 {memory_usage:.2f} MB")
         return Response(status=200)
 
-    # Update route - for updating and broadcasting
+
     @app.route("/update", methods=["GET"])
     def update():
+        """
+        Update data and optionally broadcast stock recommendations.
+
+        Headers:
+        - `API-Access-Token` (required): A token to authorize access to this API.
+        - `Target-Date` (optional): The date for retrieving data and generating stock recommendations, in "YYYY-MM-DD" format.
+        - `Need-Broadcast` (optional): A flag to determine whether the stock recommendation should be broadcasted via Line Bot.
+            - Accepts values "true" or "false" (case-insensitive). Defaults to "false" if not provided.
+
+        Responses:
+        - 200 OK: If the request is successfully processed.
+        - 400 Bad Request: If `Target-Date` has an invalid format.
+        - 401 Unauthorized: If `API-Access-Token` is missing or invalid.
+        """
         # Check if API-Access-Token header is provided
         if "API-Access-Token" not in request.headers:
             return Response("Missing API-Access-Token", status=401)
@@ -47,26 +84,17 @@ def init_routes(app):
         elif request.headers["API-Access-Token"] != current_app.config["API_ACCESS_TOKEN"]:
             return Response("Invalid API-Access-Token", status=401)
         else:
+            target_date = request.headers.get("Target-Date", None)  # with format "YYYY-MM-DD"
+            
+            if target_date:
+                try:
+                    target_date = datetime.datetime.strptime(target_date, "%Y-%m-%d").date()
+                except ValueError:
+                    return Response("Invalid Target-Date format", status=400)
+                
+            need_broadcast = request.headers.get("Need-Broadcast", "false").lower() == "true"
             logger.info("開始進行推薦")
             # Assign update and broadcast
-            update_and_broadcast_thread = threading.Thread(target=update_and_broadcast)
-            update_and_broadcast_thread.start()
-            return Response(status=200)
-        
-    # Test route - for testing usage
-    @app.route("/test", methods=["GET"])
-    def test():
-        # Check if API-Access-Token header is provided
-        if "API-Access-Token" not in request.headers:
-            return Response("Missing API-Access-Token", status=401)
-        # Check if the provided token is correct
-        elif request.headers["API-Access-Token"] != current_app.config["API_ACCESS_TOKEN"]:
-            return Response("Invalid API-Access-Token", status=401)
-        else:
-            logger.info("開始進行測試")
-            # Assign update and broadcast
-            target_date_str = request.headers["Target-Date"]  # with format "YYYY-MM-DD"
-            target_date = datetime.datetime.strptime(target_date_str, "%Y-%m-%d").date()
-            update_and_broadcast_thread = threading.Thread(target=update_and_broadcast, args=(target_date, False))
+            update_and_broadcast_thread = threading.Thread(target=update_and_broadcast, args=(target_date, need_broadcast))
             update_and_broadcast_thread.start()
             return Response(status=200)
