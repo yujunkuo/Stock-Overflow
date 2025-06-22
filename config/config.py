6a485b820a65875d9c62b8ee2160a4260940153c
yujunkuo <johnboy880313@gmail.com>
2025-06-23 02:24:32 +0800

    v5.4

--- config/config.py
+++ config/config.py
@@ -13,7 +13,7 @@ class BasicConfig:
     YEAR = "2025"
 
     # Version number
-    VERSION = "v5.3"
+    VERSION = "v5.4"
 
     # Line Bot settings
     LINE_BOT_API = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN", "default_channel_access_token"))
